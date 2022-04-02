bfc2700a0c5191fd1a9f421c30280201a9e453cb
luoxin <luoxin@dahuici.com>
2022-04-02 18:34:17 +0800

    1.redis test

---
+++ src/main/java/com/example/demo/controller/sys/RedisController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller.sys;
+
+import com.example.demo.util.entity.ResponseEntity;
+import com.example.demo.util.redis.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * redis测试
+ *
+ * @author luox
+ * @date 2022/4/2
+ */
+@RestController
+@RequestMapping("/redis")
+@Slf4j
+public class RedisController {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @GetMapping("/addRedisData")
+    public ResponseEntity<Boolean> addRedisData(@RequestParam("cacheKey") String cacheKey,
+                                                @RequestParam("cacheValue") String cacheValue){
+        redisUtil.set(cacheKey,cacheValue);
+        return ResponseEntity.success(true);
+    }
+
+    @GetMapping("/getRedisData")
+    public ResponseEntity<Object> getRedisData(@RequestParam("cacheKey") String cacheKey){
+        return ResponseEntity.success(redisUtil.get(cacheKey));
+    }
+
+
+
+
+    @GetMapping("/getRedisCache")
+    @Cacheable(value = "redisCache",key = "#cacheKey")
+    public ResponseEntity<Object> getRedisCache(@RequestParam("cacheKey") String cacheKey){
+        log.info("getRedisCache");
+        return ResponseEntity.success(redisUtil.get(cacheKey));
+    }
+
+    @GetMapping("/addRedisCache")
+    @CacheEvict(value = "redisCache",key = "#cacheKey")
+    public ResponseEntity<Boolean> addRedisCache(@RequestParam("cacheKey") String cacheKey,
+                                                 @RequestParam("cacheValue") String cacheValue){
+        log.info("addRedisCache");
+        redisUtil.set(cacheKey,cacheValue);
+        return ResponseEntity.success(true);
+    }
+
+}
